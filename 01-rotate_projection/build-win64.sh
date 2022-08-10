493358d31829e1b767578493e1c78f5acb6f79a5
chen70 <chenchenfcj@163.com>
2022-08-10 08:10:26 +0800

    modify cmake and shell for debug

--- 01-rotate_projection/build-win64.sh
+++ 01-rotate_projection/build-win64.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 
-mkdir -p build_msvc
-cd build_msvc
+mkdir -p build
+cd build
 
 cmake -G "Visual Studio 16 2019" -A x64 ../
 
-cmake --build . --config Release # 编译 Realse
+# cmake --build . --config Release # 编译 Realse
 # cmake --build . --config Debug
